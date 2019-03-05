4aa4b93dd5ecb11bdd2a8971ea1f085cdfd1cfcb
emiheskey <emmanuel@projaro.com>
2019-03-06 00:27:33 +0100

    Added form elements & Table

--- adminNew/assets/js/app.js
+++ adminNew/assets/js/app.js
@@ -110,3 +110,26 @@ function ($) {
     "use strict";
     $.MainApp.init();
 }(window.jQuery);
+
+ // Date Picker
+ jQuery('#datepicker').datepicker();
+ jQuery('#datepicker-inline').datepicker();
+ jQuery('#datepicker-multiple').datepicker({
+     numberOfMonths: 3,
+     showButtonPanel: true
+ });
+ 
+ jQuery('#datepicker').datepicker();
+ jQuery('#datepicker-autoclose').datepicker({
+     autoclose: true,
+     todayHighlight: true
+ });
+ jQuery('#datepicker-multiple-date').datepicker({
+     format: "mm/dd/yyyy",
+     clearBtn: true,
+     multidate: true,
+     multidateSeparator: ","
+ });
+ jQuery('#date-range').datepicker({
+     toggleActive: true
+ });
